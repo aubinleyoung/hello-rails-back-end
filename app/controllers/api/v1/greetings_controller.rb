30f30f2069feb1a4057db46df981bd46721a5c0d
aubinleyoung <aubinsimpeze01@gmail.com>
2023-06-07 22:17:48 +0200

    Update greeting controller

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
-    @greeting = Greeting.order('Random()').limit(1)
+    @greeting = Greeting.order('Random()').limit(5)
     render json: @greeting
   end
 end
